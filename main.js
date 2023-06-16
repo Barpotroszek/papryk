70cc18cdb17c58c1145b62b9280ed6deceb6b9dd
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2023-06-16 15:42:04 +0200

    add colors

--- main.js
+++ main.js
@@ -37,11 +37,18 @@ const lista = [
 "śpiący.jpeg",
 ]
 
+const colors = new Array(
+    "red", "#70ff70", "pink", "grey", "lightblue"
+)
+
 const container = document.querySelector(".container");
 const template = document.getElementsByTagName("template")[0];
 const item = template.content.querySelector(".wrapper")
+let i = 0;
 for (const name of lista) {
     const e = document.importNode(item, true)
+    e.style = "--color:" + colors[i%colors.length];
+    i++;
     e.querySelector("img").src = "foty/"+name+".jpeg";
     e.querySelector(".text").textContent = name + " Patryk";
     container.appendChild(e);
